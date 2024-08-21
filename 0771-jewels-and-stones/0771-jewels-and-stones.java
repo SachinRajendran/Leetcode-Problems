b4dd7ef9f27cce8751b059bdce75e06dcd5c5e33
Sachin Rajendran S <115061032+SachinRajendran@users.noreply.github.com>
2024-08-21 15:16:38 +0530

    Time: 1 ms (58.35%), Space: 41.5 MB (79.53%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -4,26 +4,26 @@ class Solution
     {
     
         int i=0,j=0,count = 0;
-
-        // for(int i=0;i<jewels.length();i++){
-        //     for(int j=0;j<stones.length();j++){
-        //         if(c[i]==c1[j])
-        //         count++;
+          
+        for( i=0;i<jewels.length();i++){
+            for( j=0;j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j))
+                count++;
+            }
+        }
+        //    while(i<jewels.length())
+        //    {
+        //     if(jewels.charAt(i)==stones.charAt(j))
+        //     {
+        //        count = count + 1;
         //     }
-        // }
-           while(i<jewels.length())
-           {
-            if(jewels.charAt(i)==stones.charAt(j))
-            {
-               count = count + 1;
-            }
-                j++;
-             if(j==stones.length())
-            {
-                i++;
-                j=0;
-            }
-           }
+        //         j++;
+        //      if(j==stones.length())
+        //     {
+        //         i++;
+        //         j=0;
+        //     }
+        //    }
         return count;
     }
 }
