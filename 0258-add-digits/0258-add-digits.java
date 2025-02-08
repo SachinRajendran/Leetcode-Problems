93784ff29a64271cfcd1aa15e7404b9141c7355d
Sachin Rajendran S <115061032+SachinRajendran@users.noreply.github.com>
2025-02-08 14:22:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int addDigits(int num)
+    {
+      int dig = 0,ans = 0;
+      while(num>10)
+      {
+       int res=0;
+      while(num>0)
+      {
+        dig = num%10;
+        res = res+dig;
+        num=num/10;
+      }
+     num = res;
+      }
+      return num;
+    }
+}
