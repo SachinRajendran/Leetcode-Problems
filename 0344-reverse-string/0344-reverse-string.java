343ec8f1f4df3a8c4b41c3fa193550cada138b26
Sachin Rajendran S <115061032+SachinRajendran@users.noreply.github.com>
2025-02-09 12:12:28 +0530

    Time: 0 ms (100%), Space: 49 MB (62.81%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public void reverseString(char[] s)
+    {
+        int left = 0,right = s.length-1;
+        char temp = '0';
+        while(left<right)
+        {
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            right--; left++;
+        }
+    }
+}
